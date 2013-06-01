f70ba349dfbc714f833a3b2df9936d6939f06e6d
deigote <diego@toharia.com>
2013-06-01 20:58:17 +0200

    Updated plugin version

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "`dirname $0`"
-find grails-app/domain/ -type f -exec touch {} \; ; grails test-app -echoOut -integration --stacktrace
+find grails-app/domain/ -type f -exec touch {} \; ; grails clean && grails test-app -echoOut -integration --stacktrace
